23fb5221d49ff0fb199771419407b963f670240d
HAOCHENYE <21724054@zju.edu.cn>
2024-07-29 08:00:29 +0000

    update dap plugin

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -15,178 +15,180 @@ return {
       hide_if_all_visible = false, -- Hides everything if all lines are visible
       throttle_ms = 100,
       handle = {
-          text = " ",
-          blend = 30, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
-          color = nil,
-          color_nr = nil, -- cterm
-          highlight = "CursorColumn",
-          hide_if_all_visible = true, -- Hides handle if all lines are visible
+        text = " ",
+        blend = 30, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
+        color = nil,
+        color_nr = nil, -- cterm
+        highlight = "CursorColumn",
+        hide_if_all_visible = true, -- Hides handle if all lines are visible
       },
       marks = {
-          Cursor = {
-              text = "•",
-              priority = 0,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "Normal",
-          },
-          Search = {
-              text = { "-", "=" },
-              priority = 1,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "Search",
-          },
-          Error = {
-              text = { "-", "=" },
-              priority = 2,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "DiagnosticVirtualTextError",
-          },
-          Warn = {
-              text = { "-", "=" },
-              priority = 3,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "DiagnosticVirtualTextWarn",
-          },
-          Info = {
-              text = { "-", "=" },
-              priority = 4,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "DiagnosticVirtualTextInfo",
-          },
-          Hint = {
-              text = { "-", "=" },
-              priority = 5,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "DiagnosticVirtualTextHint",
-          },
-          Misc = {
-              text = { "-", "=" },
-              priority = 6,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "Normal",
-          },
-          GitAdd = {
-              text = "┆",
-              priority = 7,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "GitSignsAdd",
-          },
-          GitChange = {
-              text = "┆",
-              priority = 7,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "GitSignsChange",
-          },
-          GitDelete = {
-              text = "▁",
-              priority = 7,
-              gui = nil,
-              color = nil,
-              cterm = nil,
-              color_nr = nil, -- cterm
-              highlight = "GitSignsDelete",
-          },
+        Cursor = {
+          text = "•",
+          priority = 0,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "Normal",
+        },
+        Search = {
+          text = { "-", "=" },
+          priority = 1,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "Search",
+        },
+        Error = {
+          text = { "-", "=" },
+          priority = 2,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextError",
+        },
+        Warn = {
+          text = { "-", "=" },
+          priority = 3,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextWarn",
+        },
+        Info = {
+          text = { "-", "=" },
+          priority = 4,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextInfo",
+        },
+        Hint = {
+          text = { "-", "=" },
+          priority = 5,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextHint",
+        },
+        Misc = {
+          text = { "-", "=" },
+          priority = 6,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "Normal",
+        },
+        GitAdd = {
+          text = "┆",
+          priority = 7,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "GitSignsAdd",
+        },
+        GitChange = {
+          text = "┆",
+          priority = 7,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "GitSignsChange",
+        },
+        GitDelete = {
+          text = "▁",
+          priority = 7,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "GitSignsDelete",
+        },
       },
       excluded_buftypes = {
-          "terminal",
+        "terminal",
       },
       excluded_filetypes = {
-          "cmp_docs",
-          "cmp_menu",
-          "noice",
-          "prompt",
-          "TelescopePrompt",
+        "cmp_docs",
+        "cmp_menu",
+        "noice",
+        "prompt",
+        "TelescopePrompt",
       },
       autocmd = {
-          render = {
-              "BufWinEnter",
-              "TabEnter",
-              "TermEnter",
-              "WinEnter",
-              "CmdwinLeave",
-              "TextChanged",
-              "VimResized",
-              "WinScrolled",
-          },
-          clear = {
-              "BufWinLeave",
-              "TabLeave",
-              "TermLeave",
-              "WinLeave",
-          },
+        render = {
+          "BufWinEnter",
+          "TabEnter",
+          "TermEnter",
+          "WinEnter",
+          "CmdwinLeave",
+          "TextChanged",
+          "VimResized",
+          "WinScrolled",
+        },
+        clear = {
+          "BufWinLeave",
+          "TabLeave",
+          "TermLeave",
+          "WinLeave",
+        },
       },
       handlers = {
-          cursor = true,
-          diagnostic = true,
-          gitsigns = true, -- Requires gitsigns
-          handle = true,
-          search = true, -- Requires hlslens
-          ale = false, -- Requires ALE
+        cursor = true,
+        diagnostic = true,
+        gitsigns = true, -- Requires gitsigns
+        handle = true,
+        search = true, -- Requires hlslens
+        ale = false, -- Requires ALE
       },
     },
   },
-  -- { 
+  -- {
   --     "mfussenegger/nvim-lint",
   --     opts = {
   --       linters_by_ft = { python = { 'mypy' }}
   --     }
   -- },
-  {"mg979/vim-visual-multi",
-      init = function()
+  {
+    "mg979/vim-visual-multi",
+    init = function()
       -- 在插件加载后配置键映射
-        vim.g.VM_maps = {
-          ['Find Under'] = '<C-n>',
-          ['Add Cursor Down'] = '<A-Down>',
-          ['Find Subword Under'] = '<C-n>',
-          ['Add Cursor Up'] = '<A-Up>',
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-n>",
+        ["Add Cursor Down"] = "<A-Down>",
+        ["Find Subword Under"] = "<C-n>",
+        ["Add Cursor Up"] = "<A-Up>",
       }
-    end
+    end,
   },
-  {"sindrets/diffview.nvim"},
+  { "sindrets/diffview.nvim" },
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
-        opts.auto_brackets = {}
-        -- opts.mapping.inser
-        local cmp = require("cmp")
-        opts.mapping["<Tab>"] = cmp.mapping.select_next_item()
-        opts.mapping["<S-Tab>"] = cmp.mapping.select_prev_item()
+      opts.auto_brackets = {}
+      -- opts.mapping.inser
+      local cmp = require("cmp")
+      opts.mapping["<Tab>"] = cmp.mapping.select_next_item()
+      opts.mapping["<S-Tab>"] = cmp.mapping.select_prev_item()
     end,
   },
-  {"tpope/vim-surround"},
+  { "tpope/vim-surround" },
   {
     "folke/flash.nvim",
     keys = {
-      {"S", mode={'x', 'o', 'n'}, false}
-    }
+      { "S", mode = { "x", "o", "n" }, false },
+    },
   },
+  { "mfussenegger/nvim-dap" },
   -- {
   --   "xiantang/darcula-dark.nvim"
   -- },
