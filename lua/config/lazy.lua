68c471a7e78f5e3df43147aa50437cb111aa24fe
HAOCHENYE <21724054@zju.edu.cn>
2025-02-10 12:16:16 +0800

    Update lazyvim version

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,7 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=v11.14.2", lazyrepo, lazypath })
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", version = "14" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins"},
     -- import/override with your plugins
     { import = "plugins" },
   },
